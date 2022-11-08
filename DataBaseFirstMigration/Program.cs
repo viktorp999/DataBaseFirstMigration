7e62ec4194da7f3be3e6eca06ec416fcb69c528f
viktorp999 <viktorpetrovski99@gmail.com>
2022-11-08 22:33:01 +0100

    Add project files.

---
+++ DataBaseFirstMigration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataBaseFirstMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostsDataBaseFirstEntities context = new PostsDataBaseFirstEntities();
+            Post post = new Post
+            {
+                PostID = 1,
+                DatePublished = DateTime.Now,
+                Body = "Body",
+                Title = "Title"
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
